2096dfe5da00c2d2fc2aa721d82df13c453c0c6f
Pedro Brum <pbrum191@gmail.com>
2026-05-22 14:28:42 -0300

    Enhance Tutor class with validation constraints
    Added validation annotations for fields in the Tutor class.

--- src/main/java/br/com/fiap/JornadaPet/entity/Tutor.java
+++ src/main/java/br/com/fiap/JornadaPet/entity/Tutor.java
@@ -1,6 +1,10 @@
 package br.com.fiap.JornadaPet.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -14,12 +18,22 @@ public class Tutor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+   
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
     private String nome;
 
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "E-mail deve ter formato válido")
     @Column(unique = true)
     private String email;
 
+    @Pattern(regexp = "^\\d{10,11}$", message = "Telefone deve conter 10 ou 11 dígitos")
     private String telefone;
+
+    @NotBlank(message = "CPF é obrigatório")
+    @Pattern(regexp = "^\\d{11}$", message = "CPF deve conter 11 dígitos numéricos")
     private String cpf;
+
     private String senha;
 }
