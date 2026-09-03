dac431276057f563740132e50b5355f2cdb1586f
felipeflosii <luizfelipeflosi@gmail.com>
2026-09-03 09:57:30 -0300

    fix: adiciona resiliência no EmailService e fallbacks em application.properties

--- src/main/java/br/com/fiap/VetSync/service/EmailService.java
+++ src/main/java/br/com/fiap/VetSync/service/EmailService.java
@@ -1,26 +1,33 @@
 package br.com.fiap.VetSync.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${app.mail.remetente}")
+    @Value("${app.mail.remetente:nao-responder@vetsync.com}")
     private String remetente;
 
     public void enviar(String destinatario, String assunto, String corpo) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setFrom(remetente);
-        mensagem.setTo(destinatario);
-        mensagem.setSubject(assunto);
-        mensagem.setText(corpo);
-        mailSender.send(mensagem);
+        try {
+            SimpleMailMessage mensagem = new SimpleMailMessage();
+            mensagem.setFrom(remetente);
+            mensagem.setTo(destinatario);
+            mensagem.setSubject(assunto);
+            mensagem.setText(corpo);
+            mailSender.send(mensagem);
+            log.info("E-mail enviado com sucesso para: {}", destinatario);
+        } catch (Exception e) {
+            log.warn("Não foi possível enviar e-mail para {}: {}", destinatario, e.getMessage());
+        }
     }
 }
